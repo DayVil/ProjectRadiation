f0b6e0dfef2126c10d88f5574c40d5f8aee1c598
DayVil <dayvilgp@gmail.com>
2022-05-18 16:54:36 +0200

    Added TODO

--- app/src/main/java/eu/chi/luh/projectradiation/datacollector/typecollectors/PollenCollector.kt
+++ app/src/main/java/eu/chi/luh/projectradiation/datacollector/typecollectors/PollenCollector.kt
@@ -1,12 +1,19 @@
 package eu.chi.luh.projectradiation.datacollector.typecollectors
 
 import eu.chi.luh.projectradiation.entities.Pollen
+import okhttp3.Request
+import java.util.concurrent.CountDownLatch
 
 /**
  * Only use the DataCollector for data collection purposes.
  */
 class PollenCollector(val _apiKey: String): EnvironmentCollector<Pollen>(_apiKey) {
+
+    init { //TODO: Lat and Lon to city converter.
+    }
+
     override fun collect(): Pollen? {
+        val countDownLatch = CountDownLatch(1)
         return Pollen()
     }
 }
